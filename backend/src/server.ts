ca0c154463dd4a3e5a64b54e3b7ae08a36d9cdb3
sharmaSagar01 <sagaarpkr@gmail.com>
2023-08-29 18:04:04 -0400

    Filering and Setting up the project

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,17 +1,5 @@
-import express from "express";
-import { ApolloServer } from "@apollo/server";
-import { startStandaloneServer } from "@apollo/server/standalone";
-import typeDefs from "./schema";
-import resolvers from "./resolvers";
-
 const startServer = async () => {
-  const server = new ApolloServer({ typeDefs, resolvers });
-  const { url } = await startStandaloneServer(server);
-
-  console.log(`
-  Server is running!
-  Query at ${url}
-  `);
+  console.log(`Server is running!`);
 };
 
 startServer();
