ac016ee5e9b033738867df07ebf1f750e26b9001
sharmaSagar01 <sagaarpkr@gmail.com>
2023-08-29 17:50:24 -0400

    Added Nodemon

--- backend/src/utils/utils.ts
+++ backend/src/utils/utils.ts
@@ -1,6 +1,7 @@
 const { hash, compare } = require("bcryptjs");
 
-const SALT = parseInt(process.env.SALT) || 10;
+// const SALT = parseInt(process.env.SALT) || 10;
+const SALT = 10;
 
 export const compareHash = async (password: string, hashed: string) =>
   await compare(password, hashed);
